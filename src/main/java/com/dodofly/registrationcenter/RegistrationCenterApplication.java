b34cdd6c4d9a927f6f2b7f99559c6d2e189f521c
yangzhen001 <1298605201@qq.com>
2019-08-12 18:04:04 +0800

    配置文件优化

--- src/main/java/com/dodofly/registrationcenter/RegistrationCenterApplication.java
+++ src/main/java/com/dodofly/registrationcenter/RegistrationCenterApplication.java
@@ -1,10 +1,11 @@
 package com.dodofly.registrationcenter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-import lombok.extern.slf4j.Slf4j;
+import java.util.TimeZone;
 
 @EnableEurekaServer
 @Slf4j
@@ -13,6 +14,7 @@ public class RegistrationCenterApplication {
     //http://localhost:1111/ 注册地址
     public static void main(String[] args) {
         log.info("==================RegistrationCenterApplication start .......======================");
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
         SpringApplication.run(RegistrationCenterApplication.class, args);
         log.info("==================RegistrationCenterApplication start success======================");
     }
